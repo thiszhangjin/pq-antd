fef93c413d4b2bdfa647424ceeca68a7cb67d801
zj <1099662964@qq.com>
2020-09-13 16:59:47 +0800

    🎨 [feat]调整代码结构

--- .umirc.ts
+++ .umirc.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  title: 'myAntd',
+  title: 'pq-ntd',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
-  publicPath: '/myAntd/',
-  base: '/myAntd',
+  publicPath: '/pq-antd/',
+  base: '/pq-antd',
   exportStatic: {},
   // more config: https://d.umijs.org/config
 });
